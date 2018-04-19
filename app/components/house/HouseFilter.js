9152d19eb378409cb058831a7f9eaa1253f4d5ec
Alexey Boltava <goodmoveab@gmail.com>
2018-04-19 15:34:58 +0700

    Implement house filtering page

--- app/components/house/HouseFilter.js
+++ app/components/house/HouseFilter.js
@@ -3,33 +3,58 @@ import { IProps, Icon, Tag, Intent, Card } from '@blueprintjs/core';
 import { DateRange, DateRangePicker } from "@blueprintjs/datetime";
 import { Select } from "@blueprintjs/select";
 
+import { Route, Redirect } from 'react-router-dom';
+
+import { SessionStorage, StorageKeys } from "../Storage";
+
 import moment from 'moment';
 import classNames from "classnames";
 import axios from "axios";
 
 const FORMAT = "dddd, LL";
 
-const MomentDate: React.SFC<{ date: Date; format?: string }> = ({ date, format = FORMAT }) => {
-    const m = moment(date);
-    if (m.isValid()) {
-        return <Tag intent={Intent.PRIMARY}>{m.format(format)}</Tag>;
-    } else {
-        return <Tag minimal={true}>no date</Tag>;
-    }
-};
+// const MomentDate: React.SFC<{ date: Date; format?: string }> = ({ date, format = FORMAT }) => {
+//     const m = moment(date);
+//     if (m.isValid()) {
+//         return <Tag intent={Intent.PRIMARY}>{m.format(format)}</Tag>;
+//     } else {
+//         return <Tag minimal={true}>no date</Tag>;
+//     }
+// };
+//
+// const MomentDateRange: React.SFC<{ range: DateRange; format?: string } & IProps> = ({
+//     className,
+//     range: [start, end],
+//     format = FORMAT,
+// }) => (
+//     <div className={classNames("docs-date-range", className)}>
+//         <MomentDate date={start} format={format} />
+//         <Icon icon="arrow-right" />
+//         <MomentDate date={end} format={format} />
+//     </div>
+// );
 
-const MomentDateRange: React.SFC<{ range: DateRange; format?: string } & IProps> = ({
-    className,
-    range: [start, end],
-    format = FORMAT,
-}) => (
-    <div className={classNames("docs-date-range", className)}>
-        <MomentDate date={start} format={format} />
-        <Icon icon="arrow-right" />
-        <MomentDate date={end} format={format} />
+const host = "http://172.16.16.251:5000";
+
+const dateToTimestamp = (date) => parseInt((date.getTime() / 1000).toFixed(0));
+
+const HouseCard = ({ houseId, categoryName, title, imageUrl, description, onSelect }) => (
+    <div>
+        <section>
+            <div>
+                <img src={ imageUrl } alt={ title } width={100} height={100} />
+            </div>
+            <div>
+                <h3>{ title }</h3>
+                <h4>{ categoryName }</h4>
+                <p>{ description }</p>
+            </div>
+        </section>
+        <section>
+            <button onClick={() => onSelect(houseId)} >{"Перейти к заказу"}</button>
+        </section>
     </div>
 );
-
 
 
 export default class HouseFilter extends React.Component {
@@ -43,35 +68,91 @@ export default class HouseFilter extends React.Component {
             categories: {},
             houses: {},
             links: {},
-            current_category_id: -1,
-            filtered_houses: []
+            freeHousesIds: new Set(),
+            currentCategoryId: -1,
+            filtered_houses: [],
+            redirectToOrderForm: false
         };
 
-        this.onHouseCategoryChanged = this.onHouseCategoryChanged.bind(this);
+        this.filterHousesForCategory = this.filterHousesForCategory.bind(this);
+        this.onHouseChosen = this.onHouseChosen.bind(this);
+        this.onDateRangeChanged = this.onDateRangeChanged.bind(this);
+
+        this.freeHousesRequestCancelToken = axios.CancelToken.source();
     }
 
     componentDidMount() {
-        axios("/api/form")
+        axios.get(`${host}/api/houses`)
             .then(result => this.setState(result.data))
             .catch(error => {
                 console.error(error)
             })
     }
 
-    onHouseCategoryChanged(event) {
-        console.log("house category changed to = " + event.target.value)
-        const categoryId = event.target.value
+    onDateRangeChanged(dateRange) {
+        const [rangeStart, rangeEnd] = dateRange;
+
+        if (rangeStart === null || rangeEnd === null) {
+            return;
+        }
+
+        // this.freeHousesRequestCancelToken.cancel();
+
+        axios.post(`${host}/api/free_houses`, {
+            from_date: dateToTimestamp(rangeStart),
+            to_date: dateToTimestamp(rangeEnd)
+        })
+            .then(({ data }) => {
+                this.setState({
+                    freeHousesIds: new Set(data.houses),
+                    dateRange: [rangeStart, rangeEnd]
+                }, () => this.filterHousesForCategory(this.state.currentCategoryId))
+            })
+            .catch(error => {
+                console.log(error)
+            });
+        // TODO: add "loading" spinner
+    }
+
+    filterHousesForCategory(categoryIdValue) {
+        const categoryId = Number.parseInt(categoryIdValue);
+
         this.setState({
-            current_category_id: categoryId,
-            filtered_houses: categoryId != -1 ?
-            this.state
-                .links[categoryId]
-               .map(houseId => this.state.houses[houseId]) : []
+            currentCategoryId: categoryId,
+            filtered_houses: categoryId !== -1 ?
+                this.state
+                    .links[categoryId]
+                    .filter(houseId => this.state.freeHousesIds.has(houseId))
+                    .map(houseId => this.state.houses[houseId]) : []
+        })
+    }
+
+    onHouseChosen(houseId) {
+        const house = this.state.houses[houseId];
+
+
+        console.log(this.state.dateRange);
+
+        SessionStorage.put(StorageKeys.HouseId(), houseId);
+        SessionStorage.put(StorageKeys.HouseName(), house.name);
+        SessionStorage.put(StorageKeys.CategoryId(), house.category);
+        SessionStorage.put(StorageKeys.CategoryName(), this.state.categories[house.category].name);
+        SessionStorage.put(StorageKeys.FromTimestamp(), dateToTimestamp(this.state.dateRange[0]));
+        SessionStorage.put(StorageKeys.ToTimestamp(), dateToTimestamp(this.state.dateRange[1]));
+
+        this.setState({
+            redirectToOrderForm: true
         })
     }
 
     render() {
         return (
+            this.state.redirectToOrderForm ? (
+                <Route>
+                    <Redirect push to="/react/order" />
+                </Route>
+                ) : (
+
             <div className={"houseFilter"}>
                 <Card>
                     <DateRangePicker
@@ -79,23 +160,44 @@ export default class HouseFilter extends React.Component {
                         contiguousCalendarMonths={true}
                         minDate={this.state.minDate}
                         maxDate={this.state.maxDate}
-                        onChange={(dateRange: DateRange) => this.setState({ dateRange })}
+                        onChange={this.onDateRangeChanged}
                         shortcuts={false}
                     />
 
-                    <MomentDateRange range={this.state.dateRange} />
+                    {/*<MomentDateRange range={this.state.dateRange} />*/}
 
                     <select id={"house_category_select"}
                             name={"house_category_select"}
-                            onChange={this.onHouseCategoryChanged}
+                            onChange={(event) => this.filterHousesForCategory(event.target.value)}
                             >
                         <option key={-1} value={-1}>-- Not Selected --</option>
                         { Object.values(this.state.categories).map(category => <option key={category.id} value={category.id}>{category.name}</option>) }
                     </select>
 
+                    <select id={"house_select"}
+                            name={"house_select"}
+                            disabled={this.state.currentCategoryId === -1}>
+                        <option value={-1}>-- Not Selected --</option>
+                        { this.state.filtered_houses.map(house => <option value={house.id}>{house.name}</option>) }
+                    </select>
+                </Card>
+
+                <Card>
+                    {
+                        this.state.filtered_houses.map(house =>
+                            <HouseCard houseId={house.id}
+                                       categoryName={this.state.categories[house.category].name}
+                                       title={house.name}
+                                       imageUrl={house.image_url}
+                                       description={house.description}
+                                       onSelect={this.onHouseChosen}
+                            />
+                        )
+                    }
                 </Card>
 
             </div>
+            )
         );
     }
 }

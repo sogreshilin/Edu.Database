020b58bc4f5518786052534cb30923b78777a1f0
Alexander <sogreshilin.ridder@gmail.com>
2018-05-14 02:45:50 +0700

    client category constants

---
+++ app/components/client/client.js
@@ -0,0 +1,9 @@
+export const ClientCategoryCodes = {
+    CompanyWorker: 1,
+    NonCompanyWorker: 2
+};
+
+export const ClientCategoryText = {
+    [ClientCategoryCodes.CompanyWorker]: 'Сотрудник компании',
+    [ClientCategoryCodes.NonCompanyWorker]: 'Не сотрудник компании'
+};

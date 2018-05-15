35dd8ae21c86ff94ae8041509a6d10b5c1eb4fc0
Alexander <sogreshilin.ridder@gmail.com>
2018-05-16 00:55:24 +0700

    add cli functions to recreate database from csv

--- app/cli.py
+++ app/cli.py
@@ -1,8 +1,41 @@
 import click
 import os
 
+from csv_helper import import_from_csv, export_to_csv
+
+
 def register(app):
-    # pass
+    @app.cli.group()
+    def database():
+        pass
+
+    @database.command()
+    def initialize():
+        import_from_csv('data')
+
+    @database.command()
+    @click.argument('data_dir')
+    def export_data(data_dir):
+        export_to_csv(data_dir)
+
+    @database.command()
+    @click.argument('data_dir')
+    def import_data(data_dir):
+        import_from_csv(data_dir)
+
+    @database.command()
+    def recreate():
+        if os.system('rm -rf app.db migrations'):
+            raise RuntimeError('rm old db failed')
+        if os.system('flask db init'):
+            raise RuntimeError('db init failed')
+        if os.system('flask db migrate'):
+            raise RuntimeError('db migrate failed')
+        if os.system('flask db upgrade'):
+            raise RuntimeError('flask db upgrade failed')
+        if os.system('flask database import_data data'):
+            raise RuntimeError('flask database import_data failed')
+
     @app.cli.group()
     def translate():
         """Translation and localization commands."""
--- csv_helper.py
+++ csv_helper.py
@@ -1,27 +1,55 @@
 import csv
+from sqlalchemy import create_engine, MetaData, Table, sql
+import os.path
 
-from app import db
-from app.models import HouseCategory, House, HousePrice
+
+from app.models import *
+
+model = {
+    Client: 'Client.csv',
+    HouseCategory: 'HouseCategory.csv',
+    House: 'House.csv',
+    Order: 'Order.csv',
+    Service: 'Service.csv',
+    OrderService: 'OrderService.csv',
+    Price: 'Price.csv',
+    HousePrice: 'HousePrice.csv',
+    SurchargePrice: 'SurchargePrice.csv',
+    ServicePrice: 'ServicePrice.csv',
+    PenaltyPrice: 'PenaltyPrice.csv',
+    Penalty: 'Penalty.csv',
+    Payment: 'Payment.csv',
+}
 
 
 def export_to_csv(data_dir):
-    with open(data_dir + '/house_prices.csv', 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        for current in HousePrice.query.all():
-            writer.writerow([getattr(current, column.name) for column in HousePrice.__mapper__.columns])
+    data_dir += '' if data_dir.endswith('/') else '/'
+
+    metadata = MetaData()
+    engine = create_engine('sqlite:///app.db')
+    metadata.bind = engine
+    db_connection = engine.connect()
+
+    for _class, _filename in model.items():
+        table = Table(_class.__tablename__, metadata, autoload=True)
+        select = sql.select([table])
+        result = db_connection.execute(select)
+
+        with open(data_dir + _filename, 'w') as file:
+            out_csv = csv.writer(file)
+            out_csv.writerow(result.keys())
+            out_csv.writerows(result)
+
+        print('export table', str(table).ljust(20, ' '), 'to', data_dir + _filename)
 
 
 def import_from_csv(data_dir):
-    with open(data_dir + '/house_categories.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            house_category = HouseCategory(house_category_id=int(row[0]), name=row[1], description=row[2])
-            db.session.merge(house_category)
-        db.session.commit()
+    import pandas as pd
+    data_dir += '' if data_dir.endswith('/') else '/'
+    engine = create_engine('sqlite:///app.db')
 
-    with open(data_dir + '/houses.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            house = House(house_id=int(row[0]), name=row[1], house_category_id=int(row[2]), description=row[3])
-            db.session.merge(house)
-        db.session.commit()
+    for _class, _filename in model.items():
+        if os.path.exists(data_dir + _filename):
+            df = pd.read_csv(data_dir + _filename)
+            df.to_sql(_class.__tablename__, engine, if_exists='replace')
+            print('import table', str(_class.__tablename__).ljust(20, ' '), 'from', data_dir + _filename)
